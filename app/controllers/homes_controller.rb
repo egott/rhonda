1476228181b3cdee3bf6684cf66f13e136c0b25c
Gibral Raphael <email": null>
2017-01-13 15:57:21 -0500

    completed getting start time and end time for a given day in a users calendar

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -15,8 +15,8 @@ class HomesController < ApplicationController
 
 
     # current_time = '2016-01-14T13:15:03-08:00'
-    start_date = '2017-01-14T14:15:03-08:00'
-    end_date = '2017-01-14T15:15:03-08:00'
+    start_date = '2017-01-13T01:15:03-08:00'
+    end_date = '2017-01-13T23:15:03-08:00'
 
     @list_events = {
     'max_results' => 10,
@@ -38,25 +38,39 @@ class HomesController < ApplicationController
     client.authorization.access_token = current_user.oauth_token
     service = client.discovered_api('calendar', 'v3')
 
-    @set_event = client.execute(:api_method => service.events.insert,
-                          :parameters => {'calendarId' => 'primary', 'sendNotifications' => true},
-                          :body => JSON.dump(@event),
-                          :headers => {'Content-Type' => 'application/json'})
+    # @set_event = client.execute(:api_method => service.events.insert,
+    #                       :parameters => {'calendarId' => 'primary', 'sendNotifications' => true},
+    #                       :body => JSON.dump(@event),
+    #                       :headers => {'Content-Type' => 'application/json'})
 
 
 
    ###########################################################
    # view all events
    ###########################################################
-   @events = client.execute(:api_method => service.events.list,
-                         :parameters => {'calendarId' => 'primary', 'sendNotifications' => true},
-                         :body => JSON.dump(@list_event),
-                         :headers => {'Content-Type' => 'application/json'})
-    puts @events.body
+  #  @events = client.execute(:api_method => service.events.list,
+  #                        :parameters => {'calendarId' => 'primary', 'sendNotifications' => true},
+  #                        :body => JSON.dump(@list_event),
+  #                        :headers => {'Content-Type' => 'application/json'})
+  #   puts @events.body
     # @events.each do |event|
     #   start = event.start.date || event.start.dateTime
     #   puts "- #{event.summary} (#{start})"
     # end
+
+
+    calendars = client.execute(api_method: service.events.list,
+                              :parameters => {'calendarId' => 'primary', 'sendNotifications' => true, timeMin: start_date, timeMax:end_date },
+                              :headers => {'Content-Type' => 'application/json'}
+
+
+                              )
+
+
+
+    calendars.data.items.each do |event|
+      p event.start.dateTime
+    end
 
   end
 end

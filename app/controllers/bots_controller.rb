39b0536400c1683958be0419d2e00cd5cffd3b20
Felix Vandelanotte <vandelanottefelix@gmail.com>
2017-01-14 18:48:57 -0500

    user gets a recipe based on their ingredient that they want

--- app/controllers/bots_controller.rb
+++ app/controllers/bots_controller.rb
@@ -11,24 +11,22 @@ class BotsController < ApplicationController
 
   def get_response
     response = $rhonda.text_request params["user_input"]
-    debugger
+
     if response[:result][:action] == "getRecipe"
+      $ingredient = response[:result][:parameters][:food]
       response =
               {
-                "speech": "#{get_recipe.sample}"
+                "speech": "#{get_recipe($ingredient).sample}"
               }
     elsif response[:result][:action] == "nextRecipe"
       response =
           {
-            "speech": "#{get_recipe.sample}"
+            "speech": "#{get_recipe($ingredient).sample}"
           }
     else
         response = response[:result][:fulfillment]
     end
       render json: response
   end
-
-
-
 
 end

4e603097faff1444548b74d46dd63f233d9b8c62
Emily Gottlieb <email": null>
2017-01-16 15:33:04 -0500

    add subject for giph api

--- app/helpers/bot_controller3_helper.rb
+++ app/helpers/bot_controller3_helper.rb
@@ -2,8 +2,8 @@ module BotController3Helper
   require 'net/http'
   require 'json'
 
-  def get_giph
-    url = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=kitty"
+  def get_giph(subject)
+    url = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=#{subject}"
     resp = Net::HTTP.get_response(URI.parse(url))
     buffer = resp.body
     result = JSON.parse(buffer)

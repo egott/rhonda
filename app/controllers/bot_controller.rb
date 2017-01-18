58c8409d30142c150e86c7db7365e30749764b31
Gibral Raphael <email": null>
2017-01-18 15:36:37 -0500

    added initializer for ai

--- app/controllers/bot_controller.rb
+++ app/controllers/bot_controller.rb
@@ -1,8 +1,5 @@
 class BotController < ApplicationController
   def index
-    $rhonda = ApiAiRuby::Client.new(
-    :client_access_token => "21136391b2cd47d7bbf7e5f7813287dc"
-    )
   end
 
   def get_response
